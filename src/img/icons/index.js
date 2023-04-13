34488b17835a7c2819a0565bf38ec879cbdcb3af
Ivan M <im@kalite.ro>
2023-04-13 22:10:16 +0300

    change auth

--- src/img/icons/index.js
+++ src/img/icons/index.js
@@ -1,5 +1,8 @@
 import arrow from "./arrow.png";
+import avatar from "./avatar.png";
+import lock from "./lock.png";
+import eye from "./eye.png";
 
-const el = { arrow };
+const el = { arrow, lock, avatar, eye };
 
 export default el;
